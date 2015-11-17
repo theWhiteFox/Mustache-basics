7a9a18c81af74177255cafa05aae1cdffeb58dfb
Stevo5o <stevo.joc@gmail.com>
2015-11-17 14:16:56 +0000

    Very basic Mustache with jQuery :japanese_goblin:

---
+++ scripty.js
@@ -0,0 +1,20 @@
+$(function () {
+    'use strict';
+
+
+
+    var template = '<h1>{{title}}</h1><ul>{{#names}}<li>{{name}}</li>{{/names}}</ul>';
+
+    var data = {
+        "title": "Story",
+        "names": [
+            { "name": "Tarzan" },
+            { "name": "Jane" }
+        ]
+    };
+
+    var result = Mustache.render(template, data);
+
+    document.body.innerHTML = result;
+
+});
