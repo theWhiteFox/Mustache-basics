72e016417419bf7958a070bbeef5901061d7a42a
Stevo5o <stevo.joc@gmail.com>
2015-11-18 16:05:55 +0000

    Section & Nesting :water_buffalo:

--- scripty.js
+++ scripty.js
@@ -1,9 +1,12 @@
 $(function () {
     'use strict';
     var data = {
-        name: "<strong>JavaScript</strong>",
-        year: "1995",
-        standardised: true
+        languages: [
+            { name: "<strong>JavaScript</strong>", year: "1995", developer: { firstName: "Brendan", lastName: "Eich" } },
+            { name: "<strong>Java</strong>", year: "1995", developer: { firstName: "James", lastName: "Gosling" } },
+            { name: "<strong>C#</strong>", year: "2000"},
+            { name: "<strong>PHP</strong>", year: "1998", developer: { firstName: "Rasmus", lastName: "Lerdorf" } }
+        ]
     }
 
     var template = $("#languagesTpl").html();
