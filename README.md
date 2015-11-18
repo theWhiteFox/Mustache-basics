1508ccc312f68696a30ab5d73d5270e41526cbf0
Stevo5o <stevo.joc@gmail.com>
2015-11-18 15:15:00 +0000

    Mustache basic template :tropical_fish:

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Mustache-basics
+Mustache](http://mustache.github.io/)
+
+mustache.js is an implementation of the mustache template system in JavaScript.
--- scripty.js
+++ scripty.js
@@ -1,20 +1,12 @@
 $(function () {
     'use strict';
-
-
-
-    var template = '<h1>{{title}}</h1><ul>{{#names}}<li>{{name}}</li>{{/names}}</ul>';
-
     var data = {
-        "title": "Story",
-        "names": [
-            { "name": "Tarzan" },
-            { "name": "Jane" }
-        ]
-    };
+        name: "<strong>JavaScript</strong>",
+        year: "1995",
+        standardised: true
+    }
 
-    var result = Mustache.render(template, data);
-
-    document.body.innerHTML = result;
-
+    var template = $("#languagesTpl").html();
+    var html = Mustache.to_html(template, data);
+    $('#languages').html(html);
 });
